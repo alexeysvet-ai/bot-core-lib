5b2ac8d6f53d95ab8826fb7f0ff92f098ca5882a
alexeysvet-ai <alexeysvet@gmail.com>
2026-04-05 14:56:09 +0300

    Added bot event function

--- src/bot_core/db.py
+++ src/bot_core/db.py
@@ -107,3 +107,56 @@ def set_user_lang(bot_code: str, telegram_user_id: int, lang_code: str):
     finally:
         if conn:
             conn.close()
+
+
+def insert_bot_event(
+    bot_code: str,
+    telegram_user_id: int,
+    event_type: str,
+    status: str = "success",
+    mode: str | None = None,
+    error_text_short: str | None = None,
+    file_size_bytes: int | None = None
+):
+    conn = None
+    try:
+        conn = get_connection()
+        with conn.cursor() as cur:
+            cur.execute("""
+                insert into bot_events (
+                    event_time,
+                    event_date,
+                    bot_id,
+                    telegram_user_id,
+                    event_type,
+                    status,
+                    mode,
+                    error_text_short,
+                    file_size_bytes
+                )
+                values (
+                    now(),
+                    current_date,
+                    (
+                        select id from bots where code = %s
+                    ),
+                    %s,
+                    %s,
+                    %s,
+                    %s,
+                    %s,
+                    %s
+                )
+            """, (
+                bot_code,
+                telegram_user_id,
+                event_type,
+                status,
+                mode,
+                error_text_short,
+                file_size_bytes
+            ))
+        conn.commit()
+    finally:
+        if conn:
+            conn.close()
